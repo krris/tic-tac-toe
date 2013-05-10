a4d833cacf231fc9efbd8cc0c7b7821294788c23
Krris <krrisgit@gmail.com>
2013-05-10 15:42:20 +0200

    matrix of string works

--- calc/game_status.cpp
+++ calc/game_status.cpp
@@ -3,18 +3,34 @@
 #include <boost/python.hpp>
 
 using namespace boost::python;
+using namespace std;
 
 bool draw(object grid)
 {
     return true;
 }
 
-void print_list(std::vector<std::string> list)
+void print_list(vector<string> list)
 {
-    std::vector<std::string>::iterator it;
+    vector<string>::iterator it;
     it = list.begin();
     for ( ; it != list.end(); it++)
-        std::cout << *it << std::endl;
+        cout << *it << endl;
+}
+
+
+void print_matrix(vector <vector<string> > matrix)
+{
+    vector <vector<string> >::iterator row;
+    vector<string>::iterator column;
+    for (row = matrix.begin(); row != matrix.end(); row++)
+    {
+        for (column = (*row).begin(); column != (*row).end(); column++)
+        {
+            cout << *column << " ";
+        }
+        cout << endl;
+    }
 }
 
 BOOST_PYTHON_MODULE(game_status)
@@ -22,9 +38,15 @@ BOOST_PYTHON_MODULE(game_status)
     def("draw", draw, args("grid"), "draw's docstring");
 
     def("print_list", print_list, args("list"));
+    def("print_matrix", print_matrix, args("matrix"));
     
     // vector of strings
-    class_<std::vector<std::string> >("StringVec")
-        .def(vector_indexing_suite<std::vector<std::string> >())
+    class_<vector<string> >("StringVec")
+        .def(vector_indexing_suite<vector<string> >())
+    ;
+
+    // vector of vector of strings
+    class_<vector <vector<string> > >("StringMatrix")
+        .def(vector_indexing_suite<vector <vector<string> > >())
     ;
 }
