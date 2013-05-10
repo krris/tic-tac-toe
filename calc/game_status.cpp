af1ea446d6dd027ad8f8a80856bf7b4988372c52
Krris <krrisgit@gmail.com>
2013-05-10 15:21:06 +0200

    Add string vector printing.

--- calc/game_status.cpp
+++ calc/game_status.cpp
@@ -6,13 +6,22 @@ using namespace boost::python;
 
 bool draw(object grid)
 {
-
     return true;
+}
+
+void print_list(std::vector<std::string> list)
+{
+    std::vector<std::string>::iterator it;
+    it = list.begin();
+    for ( ; it != list.end(); it++)
+        std::cout << *it << std::endl;
 }
 
 BOOST_PYTHON_MODULE(game_status)
 {
     def("draw", draw, args("grid"), "draw's docstring");
+
+    def("print_list", print_list, args("list"));
     
     // vector of strings
     class_<std::vector<std::string> >("StringVec")
